d61d5d1eb8158363c9f904aae9af47544632f3de
bugsrodriguezc <bugsrodriguezc@gmail.com>
2025-03-20 17:45:46 +0000

    Add 15.go

---
+++ src/15.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+    var x int = 5
+    var y int = 10
+
+    if x == y {
+        fmt.Println("x and y are equal")
+    } else {
+        fmt.Println("x is greater than y")
+    }
+}
