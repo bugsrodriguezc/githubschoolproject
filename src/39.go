e3b994e6d259de875bfbf72dcb0a309816030e04
bugsrodriguezc <bugsrodriguezc@gmail.com>
2025-05-20 10:34:07 +0000

    Add 39.go

---
+++ src/39.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    name := "My First GitHub Repository"
+    desc := "This is a simple example of how to create and work with a GitHub repository."
+    url := "https://github.com/your-username/Your-Project-name.git"
+
+    fmt.Printf("Name: %s\n", name)
+    fmt.Printf("Description: %s\n", desc)
+    fmt.Printf("GitHub URL: %s\n", url)
+
+    fmt.Println("This is a test message.")
+}
